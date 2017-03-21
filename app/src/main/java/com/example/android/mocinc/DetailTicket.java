6fdd2bfe5cdba3b206ba19ac6ab2cd467c195e04
khalilqibone <khalilqibran.one@gmail.com>
2017-03-21 20:08:32 +0700

    tampilan laman detail ticket

--- app/src/main/java/com/example/android/mocinc/DetailTicket.java
+++ app/src/main/java/com/example/android/mocinc/DetailTicket.java
@@ -3,11 +3,11 @@ package com.example.android.mocinc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class electronicTicket extends AppCompatActivity {
+public class DetailTicket extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_electronic_ticket);
+        setContentView(R.layout.activity_detail_ticket);
     }
 }

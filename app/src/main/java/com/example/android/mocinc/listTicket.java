17da21e751d432ef01d493b89418ae2b0d470675
muhammadfadhil <muhammad.fadhil15@gmail.com>
2017-03-25 20:42:18 +0700

    Deskrpisi Detail Tiket

---
+++ app/src/main/java/com/example/android/mocinc/listTicket.java
@@ -0,0 +1,13 @@
+package com.example.android.mocinc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_ticket);
+    }
+}

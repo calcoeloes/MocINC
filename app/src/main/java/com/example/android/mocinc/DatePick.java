60f77830c93e5dc0a95352e137ca2e98e1bcb0c7
khalilqibone <khalilqibran.one@gmail.com>
2017-03-25 23:01:14 +0700

    Membuat button (pada registrasi) dan datepicker

---
+++ app/src/main/java/com/example/android/mocinc/DatePick.java
@@ -0,0 +1,39 @@
+package com.example.android.mocinc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+public class DatePick extends AppCompatActivity{
+    DatePicker dp;
+    Button tombolOK;
+    View.OnClickListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_pick);
+        dp = (DatePicker) findViewById(R.id.datePicker1);
+        tombolOK = (Button) findViewById(R.id.btnOK);
+
+        //listener jika dilakukan klik pada tombol OK
+        listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(DatePick.this,"Tanggal: "+dp.getDayOfMonth()+
+                                "-"+(dp.getMonth()+1)+
+                                "-"+dp.getYear(),
+                        Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), Registrasi.class);
+                startActivity(i);
+            }
+        };
+
+        //menerapkan listener pada tombol OK
+        tombolOK.setOnClickListener(listener);
+    }
+}
--- app/src/main/java/com/example/android/mocinc/Registrasi.java
+++ app/src/main/java/com/example/android/mocinc/Registrasi.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
 public class Registrasi extends AppCompatActivity {
 
@@ -12,13 +12,12 @@ public class Registrasi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
-        Button button = (Button)findViewById(R.id.registrasi_button);
+
+        Button button=(Button)findViewById(R.id.searchdate);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-
-                Intent i = new Intent(getApplicationContext(), Login.class);
+                Intent i = new Intent(getApplicationContext(), DatePicker.class);
                 startActivity(i);
-
             }
         });
     }
